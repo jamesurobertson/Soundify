9176b8b7f53033f0d5f347b66baad6b0bf5f2009
ajpierskalla3 <ajpierskalla3@gmail.com>
2020-08-13 10:54:52 -0500

    change to demo user and landing page

--- frontend/main.js
+++ frontend/main.js
@@ -8,51 +8,53 @@ const app = express();
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
-
 // Define a route.
 app.get("/", (req, res) => {
-    res.render("layout");
+  res.render("layout");
 });
 
-
 app.get("/browse", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
 app.get("/browse/artist/:id(\\d+)", async (req, res) => {
-    const artistId = parseInt(req.params.id, 10)
-    res.render("home");
+  const artistId = parseInt(req.params.id, 10);
+  res.render("home");
 });
 
-app.get('/search', (req, res) => {
-    res.render("search");
-})
+app.get("/search", (req, res) => {
+  res.render("search");
+});
 
-app.get('/collection/playlist', (req, res) => {
-    res.render('collection');
-})
+app.get("/collection/playlist", (req, res) => {
+  res.render("collection");
+});
 
-app.get('/playlist/:id(\\d+)', (req, res) => {
-    // playlistId = parseInt(req.params.id)
-    // const playlists = await fetch(`localhost:8080/playlist/${playlistId}`,
-    // )
-    // res.render("playlist", { playlists })
-    res.render("playlist");
-})
+app.get("/playlist/:id(\\d+)", (req, res) => {
+  // playlistId = parseInt(req.params.id)
+  // const playlists = await fetch(`localhost:8080/playlist/${playlistId}`,
+  // )
+  // res.render("playlist", { playlists })
+  res.render("playlist");
+});
 
-app.get('/sign-up', (req, res) => {
-    res.render('sign-up');
-})
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up");
+});
 
-app.get('/log-in', (req, res) => {
-    res.render('log-in');
+app.get("/log-in", (req, res) => {
+  res.render("log-in");
+});
+
+app.get("/home-landing", (req, res) => {
+  res.render("home-landing");
 });
 
 app.get("/profile", (req, res) => {
-    res.render("profile");
+  res.render("profile");
 });
 
-app.get("/artist")
+app.get("/artist");
 
 // Define a port and start listening for connections.
 const port = 4001;
--- frontend/public/js/log-in.js
+++ frontend/public/js/log-in.js
@@ -1,61 +1,85 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const logInForm = document.getElementById("log-in-form")
+  const logInForm = document.getElementById("log-in-form");
 
-    logInForm.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        const formData = new FormData(logInForm);
-        const email = formData.get("email"); //email or username?
-        const password = formData.get("password")
-        const body = { email, password };
-        try {
-            const res = await fetch("http://localhost:8080/user/token", {
-                method: "POST",
-                body: JSON.stringify(body),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            if (!res.ok) {
-                throw res;
-            }
-            const {
-                token,
-                user: { id }
-            } = await res.json();
+  logInForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(logInForm);
+    const email = formData.get("email").fill("demo");
+    const password = formData.get("password");
+    const body = { email, password };
+    try {
+      const res = await fetch("http://localhost:8080/user/token", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!res.ok) {
+        throw res;
+      }
+      const {
+        token,
+        user: { id },
+      } = await res.json();
 
-            // document.querySelector('.header__profile-name')
-            //     .innerHTML = `${userName}`
+      // document.querySelector('.header__profile-name')
+      //     .innerHTML = `${userName}`
 
-            localStorage.setItem("SOUNDIFY_ACCESS_TOKEN", token);
-            localStorage.setItem("SOUNDIFY_CURRENT_USER_ID", id);
-            window.location.href = "/#/browse";
-        } catch (err) {
-            if (err.status >= 400 && err.status < 600) {
-                const errorJSON = await err.json();
-                const errorsContainer = document.querySelector(".errors-container");
-                let errorsHtml = [
-                    `
+      localStorage.setItem("SOUNDIFY_ACCESS_TOKEN", token);
+      localStorage.setItem("SOUNDIFY_CURRENT_USER_ID", id);
+      window.location.href = "/#/browse";
+    } catch (err) {
+      if (err.status >= 400 && err.status < 600) {
+        const errorJSON = await err.json();
+        const errorsContainer = document.querySelector(".errors-container");
+        let errorsHtml = [
+          `
                   <div class="alert alert-danger">
                       Something went wrong. Please try again.
                   </div>
                 `,
-                ];
-                const { errors } = errorJSON;
-                if (errors && Array.isArray(errors)) {
-                    errorsHtml = errors.map(
-                        (message) => `
+        ];
+        const { errors } = errorJSON;
+        if (errors && Array.isArray(errors)) {
+          errorsHtml = errors.map(
+            (message) => `
                     <div class="alert alert-danger">
                         ${message}
                     </div>
                   `
-                    );
-                }
-                errorsContainer.innerHTML = errorsHtml.join("");
-            } else {
-                alert(
-                    "Something went wrong. Please check your internet connection and try again!"
-                );
-            }
+          );
         }
-    })
-})
+        errorsContainer.innerHTML = errorsHtml.join("");
+      } else {
+        alert(
+          "Something went wrong. Please check your internet connection and try again!"
+        );
+      }
+    }
+  });
+
+  demoButton.addEventListener("click", async (e) => {
+    const body = { email: "demo@demo.com", password: "demo" };
+
+    const res = await fetch(`http://localhost:8080/user/token`, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error("this is an error");
+    }
+    const {
+      token,
+      user: { id },
+    } = await res.json();
+    // storage access_token in localStorage:
+    localStorage.setItem("SOUNDIFY_ACCESS_TOKEN", token);
+    localStorage.setItem("SOUNDIFY_CURRENT_USER_ID", id);
+    // redirect to music player
+    window.location.href = "/#/browse";
+  });
+});
